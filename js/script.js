4c13780bdec59c9014d559da2b39ae921731d402
yukuduri <duriansmile3960@gmail.com>
2018-04-01 00:32:53 +0900

    サイト正式公開

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 //外部サイトにジャンプする
 function jump(URL){
-  if( confirm("外部サイトを開こうとしています。よろしいですか？リンク先：" + URL) == true){
+  if(URL.indexOf("https://docs.google.com/forms/")==0){
+    if( confirm("Googleフォームを開こうとしています。よろしいですか？") == true){
+      window.open(URL);
+    }
+  }else if( confirm("外部サイトを開こうとしています。よろしいですか？リンク先：" + URL) == true){
     window.open(URL);
   }
+  
 }
 
 //サイト内移動をする
