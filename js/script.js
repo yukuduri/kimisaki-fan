5b6247ff9c62346ba61676841dd6902084bc3a73
yukuduri <duriansmile3960@gmail.com>
2018-05-13 15:32:56 +0900

    掲示板へのリンクを追加
    その他、細かな修正を行いました。

--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
 //外部サイトにジャンプする
 function jump(URL){
-  if(URL.indexOf("https://docs.google.com/forms/")==0){
-    if( confirm("Googleフォームを開こうとしています。よろしいですか？") == true){
-      window.open(URL);
-    }
-  }else if( confirm("外部サイトを開こうとしています。よろしいですか？リンク先：" + URL) == true){
-    window.open(URL);
-  }
-  
+  window.open(URL);
 }
 
 //サイト内移動をする
