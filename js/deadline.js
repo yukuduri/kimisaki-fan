fe3c1ea027b88fa6b8ac160846bccc5c9f668792
yukuduri <duriansmile3960@gmail.com>
2018-05-19 23:23:26 +0900

    北川ゆき合同イラ本募集要項修正
    他、deadline.jsに関して修正を行いました

--- js/deadline.js
+++ js/deadline.js
@@ -1,8 +1,9 @@
-weeks = new Array("日", "月", "火", "水", "木", "金", "土");
+var weeks = new Array("日", "月", "火", "水", "木", "金", "土"), deadline = new Array(2018, 8, 31), runDate = new Date(deadline[0], deadline[1]-1, deadline[2]), msg, currentDate, msec, dayLeft;
 
-function currentDateAndTime(){
+function deadlineCountDown(){
+  
   currentDate = new Date();
-  msg = "現在時刻："
+  msg = "現在時刻：";
   msg += currentDate.getFullYear() + "年";
   msg += (currentDate.getMonth()+1) + "月";
   msg += currentDate.getDate() + "日";
@@ -11,26 +12,16 @@ function currentDateAndTime(){
   msg += currentDate.getMinutes() + "分";
   msg += currentDate.getSeconds() +"秒";
   document.getElementById("currentDateView").innerHTML = msg;
-}
-
-function deadlineCountDown(){
-  now = new Date();
-  deadlineYear　= 2018;
-  deadlineMonth = 8;
-  deadlineDate = 31;
-  runDate = new Date(deadlineYear, deadlineMonth-1, deadlineDate);
-  msec = runDate.getTime() - now.getTime();
+  
+  msec = runDate.getTime() - currentDate.getTime();
   dayLeft = Math.floor(msec / (1000*60*60*24));
-  var msg;
   if(dayLeft > 0)
-    msg = "締切まであと" + dayLeft + "日";
+    msg = "入稿締切まであと" + dayLeft + "日";
   else if (dayLeft == 0)
-    msg = "本日は締切日です。";
+    msg = "本日は入稿締切日です。";
   else
     msg = "締切日から" + dayLeft*(-1) + "日経過しています。"
   document.getElementById("deadlineView").innerHTML = msg;
 }
 
-
-setInterval('currentDateAndTime()', 1000);
 setInterval('deadlineCountDown()', 1000);
