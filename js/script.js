3704f658dc5a41ef4770210407ed10a84efc5c79
yukuduri <duriansmile3960@gmail.com>
2018-06-22 03:06:50 +0900

    フッターに最終更新日時追加

--- js/script.js
+++ js/script.js
@@ -18,6 +18,8 @@ function footer(option){
     html = '<p>当ページは、Happy Elements株式会社「あんさんぶるガールズ！」の画像を利用しております。<br>該当画像の転載・配布等は禁止しております。<br>©Happy Elements K.K<br><br><br>©2018 kimisaki-fan.net</p>';
   }else if(option=='新旧ガル'){
     html = '<p>当ページは、Happy Elements株式会社「あんさんぶるガールズ！」「あんさんぶるガールズ！！」の画像を利用しております。<br>該当画像の転載・配布等は禁止しております。<br>©Happy Elements K.K<br><br><br>©2018 kimisaki-fan.net</p>';
+  }else if(option=='last-update'){
+    html = '<p>last-update：'+document.lastModified+'<br><br>©2018 kimisaki-fan.net</p>';
   }
   document.write(html);
 }
