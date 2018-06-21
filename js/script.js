77fc1a03899c32c847a10ed287fb108147f3f8e2
yukuduri <duriansmile3960@gmail.com>
2018-06-22 02:54:41 +0900

    試験的にフッターをJavascriptで描画
    （1引数時のJavascriptオーバーロードのテスト）

--- js/script.js
+++ js/script.js
@@ -7,3 +7,17 @@ function jump(URL){
 function move(fileName){
   location.href = fileName;
 }
+
+function footer(option){
+  var html="";
+  if(option==null){
+    html = '<p>©2018 kimisaki-fan.net</p>';
+  }else if(option=='新ガル') {
+    html = '<p>当ページは、Happy Elements株式会社「あんさんぶるガールズ！！」の画像を利用しております。<br>該当画像の転載・配布等は禁止しております。<br>©Happy Elements K.K<br><br><br>©2018 kimisaki-fan.net</p>';
+  }else if(option=='旧ガル'){
+    html = '<p>当ページは、Happy Elements株式会社「あんさんぶるガールズ！」の画像を利用しております。<br>該当画像の転載・配布等は禁止しております。<br>©Happy Elements K.K<br><br><br>©2018 kimisaki-fan.net</p>';
+  }else if(option=='新旧ガル'){
+    html = '<p>当ページは、Happy Elements株式会社「あんさんぶるガールズ！」「あんさんぶるガールズ！！」の画像を利用しております。<br>該当画像の転載・配布等は禁止しております。<br>©Happy Elements K.K<br><br><br>©2018 kimisaki-fan.net</p>';
+  }
+  document.write(html);
+}
